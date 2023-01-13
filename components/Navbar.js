5f6d603252b7b6f34a878bae371270b113bfa966
alu0101239187 <alu0101239187@ull.edu.es>
2023-01-13 20:04:18 +0000

    Mejora de diseño Stands y links blancos Navbar
    Co-authored-by: Daniel García Hernández <alu0101353340@ull.edu.es>

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,50 +6,6 @@ import styled from "@emotion/styled"
 import Link from "next/link";
 import { NavDropdown} from 'react-bootstrap';
 
-const Navbar2 = ({ navLinks }) => {
-  let ScrollLink = Scroll.Link;
-  return (
-    <Toolbar
-      component="nav"
-      sx={{
-        display: { xs: `none`, md: `flex` },
-      }}
-    >
-      <Stack direction="row" spacing={4}>
-        {navLinks.map(({ title, path }, i) => (
-          (<Link key={`${title}${i}`} href={path} className="nav-link">
-
-            {title}
-          </Link>)
-        ))}
-      </Stack>
-    </Toolbar>
-  );
-};
-
-const SpanLink = styled.span`
-  color: white;
-  &:hover {
-    color: #FF645F;
-  }
-`;
-
-const NavbarLink = styled(Link)`
-    text-decoration: none;
-    color: #FFFFFF;
-
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
-        color: #FFFFFF;
-    }
-`;
-
-const linkStyle = {
-  margin: "1rem",
-  color: "#FFFFFF",
-  textDecoration: "none",
-};
-
 const Navbar = ({ navLinks }) => {
   return (
     <Toolbar
@@ -59,15 +15,15 @@ const Navbar = ({ navLinks }) => {
       }}
     >
       <Stack direction="row" spacing={4}>
-        <NavbarLink href="/stands" className="nav-link">Stands</NavbarLink>
-        <Link href="/actividades" className="nav-link" style={linkStyle}>Actividades</Link>
+        <Link href="/stands" className="nav-link"><a className="nav-link">Stands</a></Link>
+        <Link href="/actividades" className="nav-link"><a className="nav-link">Actividades</a></Link>
 
         <NavDropdown title="El evento" id="collasible-nav-dropdown">
           <NavDropdown.Item href="/organizadores">Organizadores</NavDropdown.Item>
           <NavDropdown.Item href="/empresas">Empresas</NavDropdown.Item>
         </NavDropdown>
-        <Link href="/faqs" className="nav-link">FAQs</Link>
-        <Link href="/contacto" className="nav-link">Contacto</Link>
+        <Link href="/faqs" className="nav-link"><a className="nav-link">FAQs</a></Link>
+        <Link href="/contacto" className="nav-link"><a className="nav-link">Contacto</a></Link>
       </Stack>
     </Toolbar>
   );
