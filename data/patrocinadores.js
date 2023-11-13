2eb6e7f2ebfbc1eb3257df57cc3cec4ea0c0eab0
alu0101239187 <alu0101239187@ull.edu.es>
2023-11-13 11:08:02 +0000

    Minor changes

--- data/patrocinadores.js
+++ data/patrocinadores.js
@@ -1,135 +1,135 @@
 export const patrocinadores = [
-    {
-        src: '/images/patrocinadores/catedra_cajasiete_bob.svg',
-        alt: 'Cátedra Cajasiete Big Data, Open Data y Blockchain',
-        width: 1000,
-        height: 1000,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/catedra_ciberseguridad_binter.png',
-        alt: 'Cátedra Institucional en Ciberseguridad Binter',
-        width: 1969,
-        height: 689,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/catedra_ceoe.png',
-        alt: 'Cátedra CEOE - Tenerife Empresa y Relaciones Laborales',
-        width: 2001,
-        height: 661,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/catedra_edosoft.png',
-        alt: 'Cátedra Edosof',
-        width: 428,
-        height: 110,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/coititif.png',
-        alt: 'Colegio Oficial de Graduados e Ingenieros Técnicos Industriales de Santa Cruz de Tenerife',
-        width: 2611,
-        height: 808,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/evm.svg',
-        alt: 'EVM',
-        width: 1000,
-        height: 1000,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/intelequia.svg',
-        alt: 'Intelequia',
-        width: 1000,
-        height: 1000,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/lean_mind.png',
-        alt: 'Lean Mind',
-        width: 705,
-        height: 619,
-        types: 'oro'
-    },
-    {
-        src: '/images/patrocinadores/atlantis.jpg',
-        alt: 'Atlantis',
-        width: 2406,
-        height: 294,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/catedra_cepsa.jpg',
-        alt: 'Cátedra Fundación CEPSA',
-        width: 3453,
-        height: 2668,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/coitic.png',
-        alt: 'Colegio Profesional de Ingenieros Técnicos en Informática de Canarias',
-        width: 370,
-        height: 136,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/fecam.svg',
-        alt: 'Federación Canaria de Municipios',
-        width: 1000,
-        height: 1000,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/inetel.jpg',
-        alt: 'Inetel',
-        width: 7000,
-        height: 4000,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/mutua_tinerfena.png',
-        alt: 'Mutua Tinerfeña',
-        width: 2526,
-        height: 1786,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/open_canarias.svg',
-        alt: 'Open Canarias',
-        width: 1000,
-        height: 1000,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/tecalliance.png',
-        alt: 'TecAlliance',
-        width: 2248,
-        height: 587,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/terclima.svg',
-        alt: 'Terclima',
-        width: 1000,
-        height: 1000,
-        types: 'platino'
-    },
-    {
-        src: '/images/patrocinadores/secret_source.svg',
-        alt: 'Secret Source',
-        width: 1000,
-        height: 1000,
-        types: 'diamante'
-    },
-    {
-        src: '/images/patrocinadores/tagua.jpg',
-        alt: 'Tagua',
-        width: 551,
-        height: 189,
-        types: 'diamante'
-    }
-]
+  {
+    src: "/images/patrocinadores/catedra_cajasiete_bob.svg",
+    alt: "Cátedra Cajasiete Big Data, Open Data y Blockchain",
+    width: 1000,
+    height: 1000,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/catedra_ciberseguridad_binter.png",
+    alt: "Cátedra Institucional en Ciberseguridad Binter",
+    width: 1969,
+    height: 689,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/catedra_ceoe.png",
+    alt: "Cátedra CEOE - Tenerife Empresa y Relaciones Laborales",
+    width: 2001,
+    height: 661,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/catedra_edosoft.png",
+    alt: "Cátedra Edosof",
+    width: 428,
+    height: 110,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/coititif.png",
+    alt: "Colegio Oficial de Graduados e Ingenieros Técnicos Industriales de Santa Cruz de Tenerife",
+    width: 2611,
+    height: 808,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/evm.svg",
+    alt: "EVM",
+    width: 1000,
+    height: 1000,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/intelequia.svg",
+    alt: "Intelequia",
+    width: 1000,
+    height: 1000,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/lean_mind.png",
+    alt: "Lean Mind",
+    width: 705,
+    height: 619,
+    types: "oro",
+  },
+  {
+    src: "/images/patrocinadores/atlantis.jpg",
+    alt: "Atlantis",
+    width: 2406,
+    height: 294,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/catedra_cepsa.jpg",
+    alt: "Cátedra Fundación CEPSA",
+    width: 3453,
+    height: 2668,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/coitic.png",
+    alt: "Colegio Profesional de Ingenieros Técnicos en Informática de Canarias",
+    width: 370,
+    height: 136,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/fecam.svg",
+    alt: "Federación Canaria de Municipios",
+    width: 1000,
+    height: 1000,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/inetel.jpg",
+    alt: "Inetel",
+    width: 7000,
+    height: 4000,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/mutua_tinerfena.png",
+    alt: "Mutua Tinerfeña",
+    width: 2526,
+    height: 1786,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/open_canarias.svg",
+    alt: "Open Canarias",
+    width: 1000,
+    height: 1000,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/tecalliance.png",
+    alt: "TecAlliance",
+    width: 2248,
+    height: 587,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/terclima.svg",
+    alt: "Terclima",
+    width: 1000,
+    height: 1000,
+    types: "platino",
+  },
+  {
+    src: "/images/patrocinadores/secret_source.svg",
+    alt: "Secret Source",
+    width: 1000,
+    height: 1000,
+    types: "diamante",
+  },
+  {
+    src: "/images/patrocinadores/tagua.jpg",
+    alt: "Tagua",
+    width: 551,
+    height: 189,
+    types: "diamante",
+  },
+];
