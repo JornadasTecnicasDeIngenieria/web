63e3fb58221980ff4a01ff9e83e38fa73dbfda67
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2023-11-13 08:55:24 +0000

    Better landing image

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-import InfoJTI from '@components/home/InfoJTI';
-import Sections from '@components/home/Sections';
-import Axis from '@components/home/Axis';
-import Locate from '@components/home/Locate';
+import InfoJTI from "@components/home/InfoJTI";
+import Sections from "@components/home/Sections";
+import Axis from "@components/home/Axis";
+import Locate from "@components/home/Locate";
 import Landing from "@components/Landing";
 import Layout from "@layouts/MainLayout";
 
 const Homepage = () => {
   return (
     <Layout>
-      <Landing imgSrc="/images/landing.jpg" imgAlt="JTI" title="Jornadas Técnicas de Ingeniería" subtitle="Descubre el mundo laboral de la ingeniería" />
+      <Landing
+        imgSrc="/images/landing.png"
+        imgAlt="JTI"
+        title="Jornadas Técnicas de Ingeniería"
+        subtitle="Descubre el mundo laboral de la ingeniería"
+      />
       <InfoJTI />
       <Sections />
       <Axis />
