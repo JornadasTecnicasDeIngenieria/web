b0580094d945b7d7c7ef1ce528c08bbe44bfcd09
Saúl <alu0101405810@ull.edu.es>
2023-12-01 12:31:39 +0000

    Done

--- components/StandList.js
+++ components/StandList.js
@@ -6,81 +6,95 @@ export const StandList14 = ({ content }) => {
     <TableContainer>
       <ScrollableTable>
         <tbody>
-        <tr>
-          <th></th>
-          <TH>Martes 14</TH>
-        </tr>
-        {content.map(d => (
-          <tr key={d.stand+d.d1}>
-            <TD key={d.stand}>{d.stand}</TD>
-            <TDH key={d.d1}>{d.d1}</TDH>
+          <tr>
+            <th></th>
+            <TH>Martes 14</TH>
           </tr>
-        )
-        )}
+          {content.map((d) => (
+            <tr key={d.stand + d.d1}>
+              <TD key={d.stand}>{d.stand}</TD>
+              <TDH key={d.d1}>{d.d1}</TDH>
+            </tr>
+          ))}
         </tbody>
       </ScrollableTable>
     </TableContainer>
-  )
-}
+  );
+};
 
 export const StandList15 = ({ content }) => {
   return (
     <TableContainer>
       <ScrollableTable>
         <tbody>
-        <tr>
-          <th></th>
-          <TH>Miércoles 15</TH>
-        </tr>
-        {content.map(d => (
-          <tr key={d.stand+d.d2}>
-            <TD key={d.stand}>{d.stand}</TD>
-            <TDH key={d.d2}>{d.d2}</TDH>
+          <tr>
+            <THForSmallScreens></THForSmallScreens>
+            <TH>Miércoles 15</TH>
           </tr>
-        )
-        )}
+          {content.map((d) => (
+            <tr key={d.stand + d.d2}>
+              <TDForSmallScreens key={d.stand}> {d.stand}</TDForSmallScreens>
+              <TDH key={d.d2}>{d.d2}</TDH>
+            </tr>
+          ))}
         </tbody>
       </ScrollableTable>
     </TableContainer>
-  )
-}
+  );
+};
 
 const TH = styled.th`
-padding: 1rem;
-text-align: center;
-min-width: 10%;
-background-color: #7454ae;
-color: white;
-border: 5px solid #3c3c54;
-font-size: 1.3rem;
+  padding: 1rem;
+  text-align: center;
+  min-width: 10%;
+  background-color: #7454ae;
+  color: white;
+  border: 5px solid #3c3c54;
+  font-size: 1.3rem;
 `;
 
 const TD = styled.td`
-padding: 1rem;
-text-align: center;
-min-width: 10%;
-border: 5px solid #3c3c54;
-background: #7454ae;
-color: white;
-font-weight: bold;
-font-size: 1.3rem;
+  padding: 1rem;
+  text-align: center;
+  min-width: 10%;
+  border: 5px solid #3c3c54;
+  background: #7454ae;
+  color: white;
+  font-weight: bold;
+  font-size: 1.3rem;
 `;
 
 const TDH = styled.td`
-padding: 1rem 4rem;
-text-align: center;
-min-width: 10%;
-border: 5px solid #3c3c54;
-background-color: #FFEB83;
-font-weight: bold;
-font-size: 1.3rem;
-color: #3c3c54
+  padding: 1rem 4rem;
+  text-align: center;
+  min-width: 10%;
+  border: 5px solid #3c3c54;
+  background-color: #ffeb83;
+  font-weight: bold;
+  font-size: 1.3rem;
+  color: #3c3c54;
 `;
 
 const ScrollableTable = styled.table`
-border-collapse: collapse;
+  width: 100%;
+  border-collapse: collapse;
 `;
 
 const TableContainer = styled.div`
-overflow-x: auto;
+  overflow-x: auto;
+`;
+
+const TDForSmallScreens = styled(TD)`
+  display: none;
+  @media (max-width: 600px) {
+    display: table-cell;
+  }
+`;
+
+const THForSmallScreens = styled(TH)`
+  display: none;
+  @media (max-width: 600px) {
+    display: table-cell;
+    opacity: 0;
+  }
 `;
--- pages/stands.js
+++ pages/stands.js
@@ -119,14 +119,10 @@ const Stands = () => {
           <br />
           <br />
           <AlignedDiv>
-            <StandList14 content={stands} />
-          </AlignedDiv>
-          <br />
-          <br />
-          <br />
-          <br />
-          <AlignedDiv>
-            <StandList15 content={stands} />
+            <StandsListGrid>
+              <StandList14 content={stands} />
+              <StandList15 content={stands} />
+            </StandsListGrid>
           </AlignedDiv>
           <br />
           <br />
@@ -202,4 +198,13 @@ const HexagonB = styled.span`
   padding-right: 2rem;
   font-weight: 700;
   font-size: 130%;
+`;
+
+const StandsListGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr; /* Dos columnas, cada una ocupa la mitad del ancho disponible */
+  gap: 16px; /* Espacio entre las columnas (ajusta según sea necesario) */
+  @media (max-width: 600px) {
+    display: block;
+  }
 `;
