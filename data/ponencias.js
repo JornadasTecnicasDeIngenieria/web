4032d77ea8437ad025ca63a9cf6bddb571978acb
Saúl <alu0101405810@ull.edu.es>
2023-12-08 19:47:13 +0000

    mc

--- data/ponencias.js
+++ data/ponencias.js
@@ -1,26 +1,26 @@
 export const ponencias = [
   {
-    hora: "12:00 - 12:30",
+    hora: "12:00-12:30",
     d1: "Por determinar",
     d2: "Por determinar",
   },
   {
-    hora: "12:35 - 13:05",
+    hora: "12:35-13:05",
     d1: "Por determinar",
     d2: "Por determinar",
   },
   {
-    hora: "16:00 - 16:30",
+    hora: "16:00-16:30",
     d1: "Por determinar",
     d2: "Por determinar",
   },
   {
-    hora: "16:35 - 17:05",
+    hora: "16:35-17:05",
     d1: "Por determinar",
     d2: "Por determinar",
   },
   {
-    hora: "17:10 - 17:40",
+    hora: "17:10-17:40",
     d1: "Por determinar",
     d2: "Por determinar",
   },
