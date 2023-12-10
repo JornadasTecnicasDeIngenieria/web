79cef7dd2626ace8740ed50236f77db9442655d9
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2023-12-10 17:19:07 +0000

    Added staff

--- components/Landing.js
+++ components/Landing.js
@@ -35,8 +35,8 @@ const Landing = ({ imgSrc, imgAlt, title, subtitle }) => {
               <Image
                 src="/images/slogan.png"
                 alt="JTI"
-                width={1996}
-                height={430}
+                width={1000}
+                height={215}
                 style={{
                   width: "100%",
                   height: "100%",
