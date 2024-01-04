52e5e52e3afaea0936c9f3c967b4fee7945265e0
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2024-01-04 11:57:33 +0000

    Added sponsors

--- pages/patrocinadores.js
+++ pages/patrocinadores.js
@@ -28,7 +28,12 @@ const Patrocinadores = () => {
           <br />
           <br />
           <br />
-          <Grid container spacing={3} justify="flex-end" alignItems="center">
+          <Grid
+            container
+            spacing={3}
+            alignItems="center"
+            justifyContent="center"
+          >
             {patrocinadores.map(({ src, alt, width, height, types }) => {
               return types === "diamante" ? (
                 <Grid item xs={12} sm={12} md={6} lg={6}>
@@ -62,7 +67,12 @@ const Patrocinadores = () => {
           </AlignedDiv>
           <br />
           <br />
-          <Grid container spacing={3} justify="flex-end" alignItems="center">
+          <Grid
+            container
+            spacing={3}
+            alignItems="center"
+            justifyContent="center"
+          >
             {patrocinadores.map(({ src, alt, width, height, types }) => {
               return types === "platino" ? (
                 <Grid item xs={12} sm={12} md={6} lg={6}>
@@ -96,9 +106,53 @@ const Patrocinadores = () => {
           </AlignedDiv>
           <br />
           <br />
-          <Grid container spacing={3} justify="flex-end" alignItems="center">
+          <Grid
+            container
+            spacing={3}
+            alignItems="center"
+            justifyContent="center"
+          >
             {patrocinadores.map(({ src, alt, width, height, types }) => {
               return types === "oro" ? (
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <ImageDiv>
+                    <Image
+                      src={src}
+                      alt={alt}
+                      width={width}
+                      height={height}
+                      style={{ width: "80%", height: "80%" }}
+                    />
+                  </ImageDiv>
+                </Grid>
+              ) : (
+                ""
+              );
+            })}
+          </Grid>
+          <br />
+          <br />
+          <br />
+          <AlignedDiv>
+            <Hexagons>
+              <HexagonP>⬢&nbsp;&nbsp;</HexagonP>
+              <HexagonP>⬢&nbsp;&nbsp;</HexagonP>
+              <HexagonP>⬢</HexagonP>
+            </Hexagons>
+          </AlignedDiv>
+          <AlignedDiv>
+            <Category>COLABORADORES</Category>
+          </AlignedDiv>
+          <br />
+          <br />
+          <Grid
+            container
+            spacing={3}
+            alignItems="center"
+            justifyContent="center"
+          >
+            {patrocinadores.map(({ src, alt, width, height, types }) => {
+              return types === "colaborador" ? (
                 <Grid item xs={12} sm={12} md={6} lg={6}>
                   <ImageDiv>
                     <Image
@@ -129,13 +183,6 @@ const AlignedDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const RoundedDiv = styled.div`
-  border-radius: 20px;
-  overflow: hidden;
-  width: 560px;
-  height: 315px;
 `;
 
 const Section = styled.div`
@@ -186,6 +233,13 @@ const HexagonO = styled.span`
 
 const HexagonB = styled.span`
   color: #83ddff;
+  font-size: 3.5rem;
+  font-weight: 700;
+  font-size: 100%;
+`;
+
+const HexagonP = styled.span`
+  color: #633cae;
   font-size: 3.5rem;
   font-weight: 700;
   font-size: 100%;
