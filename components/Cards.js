9e45940862d556e77f39a13650aa558fdd6122ab
Samuel Frías <jtiesit@ull.edu.es>
2025-09-16 10:41:10 +0100

    Refactor import statement

--- components/Cards.js
+++ components/Cards.js
@@ -1,5 +1,5 @@
 import Card from './Card';
-import { Grid } from '@mui/material/GridLegacy';
+import Grid from '@mui/material/GridLegacy';
 import styled from '@emotion/styled';
 
 function Cards({ organizadores }) {
