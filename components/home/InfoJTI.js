41e581d45ed3b716f067788b69f8cd1b54755d7c
EnriqueGD08 <alu0101550329@ull.edu.es>
2026-06-12 13:20:01 +0100

    Acutalización de video de Youtube

--- components/home/InfoJTI.js
+++ components/home/InfoJTI.js
@@ -93,7 +93,7 @@ const InfoJTI = ({ title, desc }) => {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/9ziTeq5-gns?si=W0UoHfAh0wLCftZS"
+              src="https://youtu.be/-97yHq0LJfQ?si=ni8Reb-ExF6r5SCm"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
