23c8483a6cc9554236c52b1a8bcb4439e3da4ba1
EnriqueGD08 <alu0101550329@ull.edu.es>
2026-06-12 13:23:56 +0100

    Corrección de errores

--- components/home/InfoJTI.js
+++ components/home/InfoJTI.js
@@ -93,7 +93,7 @@ const InfoJTI = ({ title, desc }) => {
             <iframe
               width="100%"
               height="100%"
-              src="https://youtu.be/-97yHq0LJfQ?si=ni8Reb-ExF6r5SCm"
+              src="https://www.youtube.com/embed/-97yHq0LJfQ"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
